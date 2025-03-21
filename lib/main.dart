8d9ce86fe10d788b9b502660f67caa8a2a84ea4a
unknown <zarinkhaaaa@gmail.com>
2025-03-21 11:31:41 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Basic Flutter App"),
+        centerTitle: true,
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.favorite, size: 50, color: Colors.red),
+            SizedBox(height: 20),
+            Text(
+              "Welcome to Flutter!",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                print("Button Pressed!");
+              },
+              child: Text("Click Me"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
